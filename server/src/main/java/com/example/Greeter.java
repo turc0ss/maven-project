a1d86e3ffbed113b780a143b5dfc3ab58817d867
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-23 10:04:23 +0300

    Changed checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,10 @@ public class Greeter {
   }
 
 /**
-* @param input a greeter name variable someone
-* @return greeting string someone
+* @param someone the name of a person
+* @return greeting string
 *
-**/
-
+*/
 public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
